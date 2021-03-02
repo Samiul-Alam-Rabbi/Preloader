5b857a7fccbd1bea3b4f5ad5ae709852a9e017b5
msar3469 <msar3469@gmail.com>
2021-03-02 19:22:58 +0600

    preloader

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).on('load', function() {
+        if($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
+        }
+    });
+});
